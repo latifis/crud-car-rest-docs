1706b031a46d5d2060b73c8f7185c7b72c5de5bc
latif <latifirfansyah@gmail.com>
2024-07-24 14:53:03 +0700

    intial

---
+++ src/test/java/com/techno/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techno.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
